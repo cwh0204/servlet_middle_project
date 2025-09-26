ff2d5e6e3327b21585a2600555902f491471127f
cwh0204 <cwh0204@nate.com>
2025-09-26 21:55:47 +0900

    config 추가
    추가

--- groo/src/main/java/com/groo/model/MemberDAOImpl.java
+++ groo/src/main/java/com/groo/model/MemberDAOImpl.java
@@ -1,50 +1,20 @@
 package com.groo.model;
 
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.TransactionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
+import org.apache.ibatis.session.SqlSession;
+import com.groo.config.SessionFactory;
 import com.groo.mapper.UserMapper;
 
-import oracle.jdbc.pool.OracleDataSource;
 
 public class MemberDAOImpl implements MemberDAO{
 
-	public static Configuration configuration;
-	public static SqlSessionFactory sqlSessionFactory;
-
-	static {
-		try {
-			OracleDataSource dataSource = new OracleDataSource();
-			dataSource.setURL("jdbc:oracle:thin:@localhost:1521:XE"); // 로컬호스트 고정
-			dataSource.setUser("c##groo"); // 데이터베이스 유저이름
-			dataSource.setPassword("java"); // 데이터베이스 비밀번호
-
-			// 2. MyBatis 환경 구성
-			TransactionFactory transactionFactory = new JdbcTransactionFactory();
-			Environment environment = new Environment("dev", transactionFactory, dataSource);
-			configuration = new Configuration(environment);
-
-			// 3. Mapper 등록
-			configuration.addMapper(UserMapper.class);
-
-			// 4. SqlSessionFactory 생성
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
-
-		} catch (Exception e) {
-
-		}
-	}
+	SessionFactory sessionFactory = new SessionFactory();
 
 	@Override
 	public void signUP(MemberDTO memberDTO) {
 		// TODO Auto-generated method stub
 		// 5. 사용자 등록 처리
-		SqlSession session = sqlSessionFactory.openSession();
+		SqlSession session = sessionFactory.getSqlSession();
 		UserMapper mapper = session.getMapper(UserMapper.class);
 		mapper.insertUser(memberDTO);
 		session.commit();
@@ -53,7 +23,7 @@ public class MemberDAOImpl implements MemberDAO{
 	@Override
 	public String login(MemberDTO memberDTO) {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
+		SqlSession session = sessionFactory.getSqlSession();
 		UserMapper mapper = session.getMapper(UserMapper.class);
 		String loginCk = mapper.loginUser(memberDTO);
 		return loginCk;
--- groo/src/main/java/com/groo/model/TeamDAOImpl.java
+++ groo/src/main/java/com/groo/model/TeamDAOImpl.java
@@ -2,49 +2,19 @@ package com.groo.model;
 
 import java.util.List;
 
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.TransactionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
+import org.apache.ibatis.session.SqlSession;
+import com.groo.config.SessionFactory;
 import com.groo.mapper.TeamMapper;
 
-import oracle.jdbc.pool.OracleDataSource;
-
 public class TeamDAOImpl implements TeamDAO{
-	public static Configuration configuration;
-	public static SqlSessionFactory sqlSessionFactory;
-
-	static {
-		try {
-			OracleDataSource dataSource = new OracleDataSource();
-			dataSource.setURL("jdbc:oracle:thin:@localhost:1521:XE"); // 로컬호스트 고정
-			dataSource.setUser("c##groo"); // 데이터베이스 유저이름
-			dataSource.setPassword("java"); // 데이터베이스 비밀번호
-
-			// 2. MyBatis 환경 구성
-			TransactionFactory transactionFactory = new JdbcTransactionFactory();
-			Environment environment = new Environment("dev", transactionFactory, dataSource);
-			configuration = new Configuration(environment);
-
-			// 3. Mapper 등록
-			configuration.addMapper(TeamMapper.class);
-
-			// 4. SqlSessionFactory 생성
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
-
-		} catch (Exception e) {
-
-		}
-	}
-
+	
+	SessionFactory sessionFactory = new SessionFactory();
+	
 	@Override
 	public void createTeam(TeamDTO teamDTO) {
 		// TODO Auto-generated method stub
-		SqlSession session = sqlSessionFactory.openSession();
+		SqlSession session = sessionFactory.getSqlSession();
 		TeamMapper mapper = session.getMapper(TeamMapper.class);
 		mapper.insertTeam(teamDTO);
 		session.commit();
@@ -54,7 +24,7 @@ public class TeamDAOImpl implements TeamDAO{
 	public List<TeamDTO> selectTeam(TeamDTO teamDTO) {
 
 		System.out.println("selectTeam = "+teamDTO.getUserId());
-		SqlSession session = sqlSessionFactory.openSession();
+		SqlSession session = sessionFactory.getSqlSession();
 		TeamMapper mapper = session.getMapper(TeamMapper.class);
 		List<TeamDTO> teamList = mapper.selectTeam(teamDTO);
 		return teamList;
@@ -62,7 +32,7 @@ public class TeamDAOImpl implements TeamDAO{
 
 	@Override
 	public List<TeamDTO> selectTeamAll() {
-		SqlSession session = sqlSessionFactory.openSession();
+		SqlSession session = sessionFactory.getSqlSession();
 		TeamMapper mapper = session.getMapper(TeamMapper.class);
 		List<TeamDTO> teamList = mapper.selectTeamAll();
 		return teamList;
