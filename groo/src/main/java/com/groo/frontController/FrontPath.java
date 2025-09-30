38959b853f4c63b3d61fb36422f31e56e721a006
cwh0204 <cwh0204@nate.com>
2025-09-30 21:58:37 +0900

    20250930
    그리드 추가

--- groo/src/main/java/com/groo/frontController/FrontPath.java
+++ groo/src/main/java/com/groo/frontController/FrontPath.java
@@ -15,6 +15,7 @@ public class FrontPath {
         jspMap.put("/teamfind.do", "/WEB-INF/view/teamfind.jsp");
         jspMap.put("/userdetail.do", "/WEB-INF/view/userDetail.jsp");
         jspMap.put("/admin.do", "/WEB-INF/view/admin.jsp");
+        jspMap.put("/adminuser.do", "/WEB-INF/view/adminUser.jsp");
     }
 
     public String getJspPath(String command) {
