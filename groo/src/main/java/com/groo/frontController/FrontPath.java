82ef5786995196dfd71d4c54bbd55aad49884bca
cwh0204 <48270074+cwh0204@users.noreply.github.com>
2025-10-08 01:33:35 +0900

    어드민 줄바꿈

--- groo/src/main/java/com/groo/frontController/FrontPath.java
+++ groo/src/main/java/com/groo/frontController/FrontPath.java
@@ -14,7 +14,6 @@ public class FrontPath {
         jspMap.put("/teamcreate.do", "/WEB-INF/view/team/teamcreate.jsp");
         jspMap.put("/teamfind.do", "/WEB-INF/view/team/teamfind.jsp");
         jspMap.put("/userdetail.do", "/WEB-INF/view/user/userDetail.jsp");
-        
         jspMap.put("/admin.do", "/WEB-INF/view/admin/admin.jsp");
         jspMap.put("/admindashboard.do", "/WEB-INF/view/admin/adminDashboard.jsp");
         jspMap.put("/adminuser.do", "/WEB-INF/view/admin/adminUser.jsp");
