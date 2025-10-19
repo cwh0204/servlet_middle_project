b46e56718588ddde02adac1742421783c890cfb9
suminu20 <suminu20@gmail.com>
2025-10-19 17:52:30 +0900

    수정

--- groo/src/main/java/com/groo/frontController/FrontPath.java
+++ groo/src/main/java/com/groo/frontController/FrontPath.java
@@ -16,7 +16,7 @@ public class FrontPath {
         jspMap.put("/signuppage.do", "/WEB-INF/view/user/signuppage.jsp");
         jspMap.put("/teamcreate.do", "/WEB-INF/view/team/teamcreate.jsp");
         jspMap.put("/teamfind.do", "/WEB-INF/view/team/teamfind.jsp");
-        jspMap.put("/userdetail.do", "/WEB-INF/view/user/userDetail.jsp");
+        jspMap.put("/userdetail.do", "/WEB-INF/view/user/userdetail.jsp");
         jspMap.put("/mypage.do", "/WEB-INF/view/user/mypage.jsp");
 
         jspMap.put("/admin.do", "/WEB-INF/view/admin/admin.jsp");
