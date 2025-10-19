f5242df07dd09ef917bf0a2a5e1e997dbca80ae7
suminu20 <suminu20@gmail.com>
2025-10-19 23:07:01 +0900

    userdetailpw생성
    수정페이지 전 비밀번호 입력

--- groo/src/main/java/com/groo/frontController/FrontPath.java
+++ groo/src/main/java/com/groo/frontController/FrontPath.java
@@ -16,6 +16,7 @@ public class FrontPath {
         jspMap.put("/signuppage.do", "/WEB-INF/view/user/signuppage.jsp");
         jspMap.put("/teamcreate.do", "/WEB-INF/view/team/teamcreate.jsp");
         jspMap.put("/teamfind.do", "/WEB-INF/view/team/teamfind.jsp");
+        jspMap.put("/userdetailpw.do", "/WEB-INF/view/user/userdetailpw.jsp");
         jspMap.put("/userdetail.do", "/WEB-INF/view/user/userdetail.jsp");
         jspMap.put("/mypage.do", "/WEB-INF/view/user/mypage.jsp");
 
