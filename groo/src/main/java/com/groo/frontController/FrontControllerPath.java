f4d8e1e7c01c82fbfb2b60fbb9a8b2b0cfbd034a
cwh0204 <cwh0204@nate.com>
2025-10-21 19:40:02 +0900

    1212
    1212

--- groo/src/main/java/com/groo/frontController/FrontControllerPath.java
+++ groo/src/main/java/com/groo/frontController/FrontControllerPath.java
@@ -25,13 +25,13 @@ public class FrontControllerPath {
     public FrontControllerPath() {
         controllerMap.put("/logins.do", new LoginImpl());
         //controllerMap.put("/index.do", new MainImpl()); // indexImpl 재구성
-        controllerMap.put("/main.do", new MainImpl());
+        controllerMap.put("/mains.do", new MainImpl());
         controllerMap.put("/singnups.do", new SignUpImpl());
         controllerMap.put("/teamcreates.do", new TeamCreateImpl());
         controllerMap.put("/teamselects.do", new TeamSelectImpl());
-        controllerMap.put("/mypage.do", new MyPageImpl());
+        controllerMap.put("/mypages.do", new MyPageImpl());
         
-        controllerMap.put("/mypage.do", new MyPageImpl());
+        controllerMap.put("/mypages.do", new MyPageImpl());
         
         controllerMap.put("/naverlogin.do", new NaverLogin());
         controllerMap.put("/githublogin.do", new GitHubLogin());
