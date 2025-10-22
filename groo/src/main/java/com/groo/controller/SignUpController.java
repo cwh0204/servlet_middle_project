2f07804d3a74d8ff49494bc3918a82d3dcb750ea
limjw9114 <limjw9114@gmail.com>
2025-10-22 20:32:36 +0900

    12233
    1233

--- groo/src/main/java/com/groo/controller/SignUpController.java
+++ groo/src/main/java/com/groo/controller/SignUpController.java
@@ -22,32 +22,6 @@ public class SignUpController extends HttpServlet implements Controller {
 
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/plain; charset=UTF-8");
-
-<<<<<<< HEAD
-//		String name = request.getParameter("userName");
-//		String userId = request.getParameter("userId");
-//		String password = request.getParameter("userPw");
-//		
-//		System.out.println(name);
-//		System.out.println(userId);
-//		System.out.println(password);
-//		
-//		MemberDTO memberDTO = new MemberDTO();
-//
-//		memberDTO.setName(name);
-//		memberDTO.setUserId(userId);
-//		memberDTO.setPassword(password);
-//
-//		try {
-//			MemberServiceImpl serviceImpl = new MemberServiceImpl();
-//			serviceImpl.insertUserService(memberDTO);
-//			response.getWriter().println("사용자 등록 완료: " + memberDTO.getUserId());
-//			/* response.sendRedirect("login.do"); */
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			response.getWriter().println("사용자 등록 실패: " + e.getMessage());
-//		}
 		
 		String memLoginId = request.getParameter("userLoginId");
 		String memPass = request.getParameter("userPass");
@@ -55,13 +29,6 @@ public class SignUpController extends HttpServlet implements Controller {
 		String memEmail = request.getParameter("userEmail");
 		String memBirth = request.getParameter("userBirth");
 		String memGender = request.getParameter("userGender");
-		
-		System.out.println(memLoginId);
-		System.out.println(memPass);
-		System.out.println(memName);
-		System.out.println(memEmail);
-		System.out.println(memBirth);
-		System.out.println(memGender);
 		
 		MemberDTO memberDto = new MemberDTO();
 		
@@ -77,31 +44,7 @@ public class SignUpController extends HttpServlet implements Controller {
 			serviceImpl.insertUserService(memberDto);
 			response.getWriter().println("회원가입 완료: "+ memberDto.getMemId());
 		}catch(Exception e) {
-=======
-		String name = request.getParameter("userName");
-		String userId = request.getParameter("userId");
-		String password = request.getParameter("userPw");
-
-		System.out.println(name);
-		System.out.println(userId);
-		System.out.println(password);
-
-		MemberDTO memberDTO = new MemberDTO();
-
-		memberDTO.setName(name);
-		memberDTO.setUserId(userId);
-		memberDTO.setPassword(password);
-
-		try {
-			MemberServiceImpl serviceImpl = new MemberServiceImpl();
-			serviceImpl.insertUserService(memberDTO);
-			response.getWriter().println("사용자 등록 완료: " + memberDTO.getUserId());
-			/* response.sendRedirect("login.do"); */
-
-		} catch (Exception e) {
->>>>>>> 3a9a40ba71211eee6914313d568effa99e774eb3
-			e.printStackTrace();
-			response.getWriter().println("회원가입 실패: "+ e.getMessage());
+			
 		}
 	}
 
