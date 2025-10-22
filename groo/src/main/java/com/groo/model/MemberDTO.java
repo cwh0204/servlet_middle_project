d78a5536a832c894cf3f5356347073095cb9d2a2
limjw9114 <limjw9114@gmail.com>
2025-10-22 10:28:52 +0900

    insert dto까지 작성
    .

--- groo/src/main/java/com/groo/model/MemberDTO.java
+++ groo/src/main/java/com/groo/model/MemberDTO.java
@@ -4,7 +4,26 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
-	private String name;
-	private String userId;
-	private String password;
+
+//	private String name;
+//	private String userId;
+//	private String password;
+
+	private String mem_id;
+	private String mem_name;
+	private String mem_login_id;
+	private String mem_pass;
+	private String mem_type;
+	private String mem_login_token;
+	private String mem_nick;
+	private int mem_birth;
+	private String mem_gender;
+	private String mem_email;
+	private String mem_img_path;
+	private String mem_phone;
+	private String mem_addr;
+	private String mem_interest;
+	private int mem_signup_date;
+	private int mem_out_date;
+	private String mem_status;
 }
