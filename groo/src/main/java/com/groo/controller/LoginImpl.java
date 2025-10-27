bae57450ec4cab97fa6de2acf5a40f3d16c8a79a
cwh0204 <cwh0204@nate.com>
2025-10-27 11:16:52 +0900

    마이페이지 재귀 오류 수정
    admin js파일 분리

--- groo/src/main/java/com/groo/controller/LoginImpl.java
+++ groo/src/main/java/com/groo/controller/LoginImpl.java
@@ -3,7 +3,9 @@ package com.groo.controller;
 import java.io.IOException;
 
 import com.groo.error.ErrorDTO;
+import com.groo.error.InternalDataAccessException;
 import com.groo.error.InternalServerErrorException;
+import com.groo.error.InternalServiceException;
 import com.groo.error.ResourceNotFoundException;
 import com.groo.model.MemberDTO;
 import com.groo.service.MemberServiceImpl;
@@ -34,8 +36,8 @@ public class LoginImpl extends HttpServlet implements Controller {
 			String password = request.getParameter("password");
 
 			MemberDTO memberDTO = new MemberDTO();
-			memberDTO.setUserId(userId);
-			memberDTO.setPassword(password);
+			memberDTO.setMemId(userId);
+			memberDTO.setMemPass(password);
 
 			try {
 				MemberServiceImpl serviceImpl = new MemberServiceImpl();
@@ -44,23 +46,16 @@ public class LoginImpl extends HttpServlet implements Controller {
 
 				if (reMemberDTO != null) {
 					HttpSession httpSession = request.getSession();
-					httpSession.setAttribute("userId", reMemberDTO.getUserId());
+					httpSession.setAttribute("userId", reMemberDTO.getMemId());
 //					System.out.println("로그인 세션 정보 "+httpSession.getAttribute("userId"));
 					response.sendRedirect("main.do");
 				} else {
 					response.getWriter().println("로그인 실패");
 					response.sendRedirect("login.do");
 				}
-			}catch(ResourceNotFoundException rne) {
-				rne.printStackTrace();
-				ErrorDTO error = new ErrorDTO(500,"회원가입 중 오류 발생","MemberDAO");
-				response.getWriter().println(error.getStatus()+error.getError());
-			}
-			catch(InternalServerErrorException ie) {
+			}catch(InternalServiceException ise) {
 				ErrorDTO error = new ErrorDTO(404,"회원가입 입력데이터 오류 발생","MemberDAO");
 				response.getWriter().println(error.getStatus()+error.getError());
-			}catch(RuntimeException re) {
-				re.printStackTrace();
 			}catch (Exception e) {
 				e.printStackTrace();
 				response.getWriter().println("로그인 실패: " + e.getMessage());
---
+++ groo/src/main/webapp/js/adminJs/adminReport.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+
+
+function initializeDisableUserGrid() {
+	reportUsergrid = new tui.Grid({
+		el: document.getElementById('reportUsergrid'),
+		data: [], // 초기 데이터는 빈 배열로 설정
+		rowKey: 'id',
+		scrollX: true,
+		scrollY: true,
+		columnOptions: {
+			resizable: true // 이 값이 false이면 리사이징 기능이 비활성화됩니다.
+		},
+		columns: [{
+			header: '신고번호',
+			name: 'reportId',
+			width: 100
+		}, {
+			header: '신고자',
+			name: 'memReporter',
+			width: 100
+		}, {
+			header: '신고아이디',
+			name: 'memSuspect',
+			width: 100
+		}, {
+			header: '글번호',
+			name: 'reportBoard',
+			width: 100
+		}, {
+			header: '신고사유',
+			name: 'reportContent'
+		}, {
+			header: 'Grade',
+			name: 'grade',
+			width: 150,
+			renderer: {
+				type: CustomReportProcessBtnRenderer,
+			}
+		}
+		]
+	});
+	reportUsergrid.setBodyHeight(450);
+}
