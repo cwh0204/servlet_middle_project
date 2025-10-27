0ab598e1f4f687270bb59abbc2e9a234576665ed
suminu20 <suminu20@gmail.com>
2025-10-27 23:50:46 +0900

    form움직
    .

--- groo/src/main/webapp/js/teamCreate.js
+++ groo/src/main/webapp/js/teamCreate.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-$('.input-daterange').datepicker({
-    format: 'dd-mm-yyyy',
-    autoclose: true
+    // 달력 초기화
+    $('.input-daterange').datepicker({
+        format: 'dd-mm-yyyy',
+        autoclose: true
+    });
+
+    // 나이 슬라이더 값 표시
+    const ageSlider = document.getElementById("teamMemberage");
+    const ageValue = document.getElementById("ageValue");
+    ageSlider.addEventListener("input", () => {
+        ageValue.textContent = ageSlider.value + "세";
+    });
 });
-
-});
-
-// 나이 슬라이더 값 표시
-   const ageSlider = document.getElementById("teamMemberage");
-   const ageValue = document.getElementById("ageValue");
-   ageSlider.addEventListener("input", () => {
-      ageValue.textContent = ageSlider.value + "세";
-   });
-
