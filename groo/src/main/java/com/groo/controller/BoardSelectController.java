c2e9d2fd00794b78e184a95df34d12a5720c05fe
cwh0204 <cwh0204@nate.com>
2025-10-28 17:24:32 +0900

    게시판 조회 쿼리문 수정
    123

--- groo/src/main/java/com/groo/controller/BoardSelectController.java
+++ groo/src/main/java/com/groo/controller/BoardSelectController.java
@@ -32,12 +32,13 @@ public class BoardSelectController implements Controller {
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String findField = request.getParameter("findField");
 		String findName = request.getParameter("findName");
-
+		String boardTypeId = request.getParameter("boardTypeId");
 		BoardDTO board = new BoardDTO();
 
 		board.setFindField(findField);
 		board.setFindName(findName);
-
+		board.setBoardTypeId(boardTypeId);
+		
 		BoardServiceImpl service = new BoardServiceImpl();
 
 		List<BoardDTO> list = new ArrayList<>();
