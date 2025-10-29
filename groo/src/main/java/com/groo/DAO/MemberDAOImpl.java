054b4b7035e5bc1817718adf2b382e84b40b5783
suminu20 <suminu20@gmail.com>
2025-10-29 19:53:29 +0900

    예외 수정
    .

--- groo/src/main/java/com/groo/DAO/MemberDAOImpl.java
+++ groo/src/main/java/com/groo/DAO/MemberDAOImpl.java
@@ -83,7 +83,7 @@ public class MemberDAOImpl implements MemberDAO{
 			session.update("userDelete", member);
 		}catch(Exception e) {
 			e.printStackTrace();
-			throw new InternalDataAccessException("DAO:userdetail 예외발생", e);
+			throw new InternalDataAccessException("DAO:userdetailpw 예외발생", e);
 		}
 	}
 
@@ -94,7 +94,7 @@ public class MemberDAOImpl implements MemberDAO{
 			session.update("userUpdate", member);
 		}catch(Exception e) {
 			e.printStackTrace();
-			throw new InternalDataAccessException("DAO:userdetailpw 예외발생", e);
+			throw new InternalDataAccessException("DAO:userdetail 예외발생", e);
 		}
 	}
 
