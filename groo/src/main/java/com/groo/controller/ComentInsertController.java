4ca72bbe5f63c4071b61df79864024a4a1f5e607
cwh0204 <cwh0204@nate.com>
2025-10-29 17:17:26 +0900

    댓글등록
    123

---
+++ groo/src/main/java/com/groo/controller/ComentInsertController.java
@@ -0,0 +1,69 @@
+package com.groo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.google.gson.Gson;
+import com.groo.error.ErrorDTO;
+import com.groo.error.InternalServiceException;
+import com.groo.model.ComentDTO;
+import com.groo.service.ComentService;
+import com.groo.service.ComentServiceImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * 댓글 등록을 처리하는 Controller 구현 클래스 Service 계층을 호출하고 처리 결과를 json 형태로
+ * 클라이언트에 응답
+ */
+public class ComentInsertController implements Controller {
+	
+	/**
+	 * HTTP 요청을 받아 회원 목록을 조회하고 JSON 응답을 생성합니다.
+	 *
+	 * @param request  HTTP 요청 객체
+	 * @param response HTTP 응답 객체
+	 * @throws ServletException 서블릿 관련 오류 발생 시
+	 * @throws IOException      입출력 오류 발생 시
+	 */
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String memLoginId = request.getParameter("memLoginId");
+		String boardId = request.getParameter("boardId");
+		String comentContent = request.getParameter("comentContent");
+		
+		ComentDTO coment = new ComentDTO();
+		coment.setMemLoginId(memLoginId);
+		coment.setBoardId(boardId);
+		coment.setComentContent(comentContent);
+		
+		ComentService service = new ComentServiceImpl();
+		try {
+
+			service.comentInsert(coment);
+
+			Gson gson = new Gson();
+			String json = gson.toJson("sucsses");
+
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+
+			PrintWriter out = response.getWriter();
+			out.print(json);
+			out.flush();
+		}catch (InternalServiceException ise) {
+			ise.printStackTrace();
+			ErrorDTO error = new ErrorDTO();
+			error.setStatus(500);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			ErrorDTO error = new ErrorDTO();
+			error.setStatus(500);
+		}
+	}
+
+}
---
+++ groo/src/main/java/com/groo/service/ComentService.java
@@ -0,0 +1,7 @@
+package com.groo.service;
+
+import com.groo.model.ComentDTO;
+
+public interface ComentService {
+	public void comentInsert(ComentDTO coment);
+}
