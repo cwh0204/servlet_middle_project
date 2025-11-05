d4109132fb5ab084f1ec7e36f75952de25a51364
cwh0204 <cwh0204@nate.com>
2025-11-05 15:30:53 +0900

    1212
    1212

--- groo/src/main/java/com/groo/DAO/VoteDAO.java
+++ groo/src/main/java/com/groo/DAO/VoteDAO.java
@@ -9,4 +9,5 @@ import com.groo.model.VoteDTO;
 public interface VoteDAO {
 	public void insertVote(VoteDTO vote, SqlSession session);
 	public List<VoteDTO> selectVote(VoteDTO vote, SqlSession session);
+	public void updateVoteOk(VoteDTO vote, SqlSession session);
 }
--- groo/src/main/java/com/groo/frontController/FrontControllerPath.java
+++ groo/src/main/java/com/groo/frontController/FrontControllerPath.java
@@ -53,6 +53,7 @@ import com.groo.controller.UserdetailSelectNicknameController;
 import com.groo.controller.UserdetailSelectUserListController;
 import com.groo.controller.VoteInsertController;
 import com.groo.controller.VoteSelectController;
+import com.groo.controller.VoteUpdateOkController;
 
 public class FrontControllerPath {
 
@@ -106,16 +107,12 @@ public class FrontControllerPath {
         controllerMap.put("/teamdelete.do", new TeamDeleteController());
         controllerMap.put("/teamactivate.do", new TeamActivateController());
         controllerMap.put("/teamselectmyteam.do", new TeamSelectMyTeamController());
-
         controllerMap.put("/selectloginid.do", new SignUpSelectLoginIdController());
         controllerMap.put("/selectemail.do", new SignUpSelectEmailController());
-
         controllerMap.put("/naverlogin.do", new NaverLoginController());
         controllerMap.put("/githublogin.do", new GitHubLoginController());
         controllerMap.put("/kakaologin.do", new KakaoLoginController());
-        controllerMap.put("/naverlogin.do", new NaverLoginController());
-        controllerMap.put("/githublogin.do", new GitHubLoginController());
-        controllerMap.put("/kakaologin.do", new KakaoLoginController());
+        controllerMap.put("/voteok.do", new VoteUpdateOkController());
 
         //신고 관련 API
         controllerMap.put("/reportinsert.do", new ReportInsertController());
