54bda909fae3b182e509c37750f7623a77914dfb
sayu95 <jeongsayu95@gmail.com>
2025-11-05 17:00:25 +0900

    asd
    asdas

--- groo/src/main/java/com/groo/frontController/FrontPath.java
+++ groo/src/main/java/com/groo/frontController/FrontPath.java
@@ -24,6 +24,7 @@ public class FrontPath {
         jspMap.put("/mypagedashboardhome.do", "/WEB-INF/view/mypage/mypagedashboard/mypagedashboardhome.jsp");
         jspMap.put("/myboard.do", "/WEB-INF/view/mypage/myboard/myboardhome.jsp");
         jspMap.put("/myreview.do", "/WEB-INF/view/mypage/myreview/myreviewhome.jsp");
+        jspMap.put("/myreviewedit.do", "/WEB-INF/view/mypage/myreview/myreviewedit.jsp");
 
         //관리자 경로
         jspMap.put("/admin.do", "/WEB-INF/view/admin/admin.jsp");
