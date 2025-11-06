ed5454fe568341d74c78ef33cc0cc25b99c57d1a
cwh0204 <cwh0204@nate.com>
2025-11-06 21:31:37 +0900

    수정완료
    수정완료

--- groo/src/main/webapp/js/team/teamNav/teamdetailNav.js
+++ groo/src/main/webapp/js/team/teamNav/teamdetailNav.js
@@ -10,12 +10,11 @@ var menuItemContent = (lastPage, SESSION_KEY) => {
 	$menuItem.addClass('active');
 
 	const pageToLoad = $menuItem.data('page');
-	console.log("메뉴 클릭, 로드할 페이지:", pageToLoad);
+	console.log("메뉴 클릭, 로드할 페이지:", DEFAULT_PAGE);
 
 	if (pageToLoad) {
 		// ⭐ 3. 페이지 클릭 시 sessionStorage에 현재 페이지 저장 ⭐
 		sessionStorage.setItem(SESSION_KEY, pageToLoad);
-		sessionStorage.setItem('main_last_view', pageToLoad);
 		
 		$('.teamcontent').load(pageToLoad, function(response, status, xhr) {
 			if (status === "error") {
@@ -28,7 +27,7 @@ var menuItemContent = (lastPage, SESSION_KEY) => {
 $(document).ready(function() {
 
 	const SESSION_KEY = 'team_detail_last_view';
-	const DEFAULT_PAGE = 'teamdetailhome.do';
+	const DEFAULT_PAGE = 'teamdetail.do';
 	
 	const test = sessionStorage.getItem(SESSION_KEY);
 	
@@ -38,7 +37,7 @@ $(document).ready(function() {
 		// 로딩 시작 전에 콘솔에 기록하고 세션에 저장 (메뉴 클릭 시)
 		if (key === SESSION_KEY) {
 			sessionStorage.setItem(key, pageToLoad);
-			sessionStorage.setItem('main_last_view', pageToLoad);
+			
 		}
 
 		$('.teamcontent').load(pageToLoad, function(response, status, xhr) {
@@ -96,7 +95,7 @@ $(document).ready(function() {
 
 		if (pageToLoad) {
 			console.log("메뉴 클릭, 로드할 페이지:", pageToLoad);
-
+			sessionStorage.setItem('main_last_view', 'teamdetail.do');
 			// UI 변경 로직 (active 상태)
 			$('.menu-item').removeClass('active');
 			$menuItem.addClass('active');
